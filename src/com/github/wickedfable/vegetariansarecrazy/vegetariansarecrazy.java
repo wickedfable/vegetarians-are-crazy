8b7a8d71a739b41f8774ede2a97475b296260e5a
wicked.fable <wicked.fable@gmail.com>
2012-11-05 20:53:23 -0800

    v0.1.2 - Two new recipes and compiled in Java 6

--- src/com/github/wickedfable/vegetariansarecrazy/vegetariansarecrazy.java
+++ src/com/github/wickedfable/vegetariansarecrazy/vegetariansarecrazy.java
@@ -12,17 +12,23 @@ public class vegetariansarecrazy extends JavaPlugin
 
 	public void onEnable()
 	{
-		getLogger().info("[Vegetarians Are Crazy][v0.1a] Plugin is active.");
+		getLogger().info("[v0.1.2] Plugin is active.");
     
 		ShapelessRecipe vegetarian_book = new ShapelessRecipe(new ItemStack(Material.BOOK, 1)).addIngredient(Material.PAPER).addIngredient(Material.PAPER).addIngredient(Material.PAPER).addIngredient(Material.WHEAT);
 		getServer().addRecipe(vegetarian_book);
+		
+		ShapelessRecipe vegetarian_pumpkinpie = new ShapelessRecipe(new ItemStack(Material.PUMPKIN_PIE, 1)).addIngredient(Material.SUGAR).addIngredient(Material.APPLE).addIngredient(Material.PUMPKIN);
+		getServer().addRecipe(vegetarian_pumpkinpie);
     
-		ShapedRecipe vegetarian_itemframe = new ShapedRecipe(new ItemStack(Material.ITEM_FRAME)).shape(new String[] { "AAA", "ABA", "AAA" }).setIngredient('A', Material.STICK).setIngredient('B', Material.WHEAT);
+		ShapedRecipe vegetarian_itemframe = new ShapedRecipe(new ItemStack(Material.ITEM_FRAME, 1)).shape(new String[] { "AAA", "ABA", "AAA" }).setIngredient('A', Material.STICK).setIngredient('B', Material.WHEAT);
 		getServer().addRecipe(vegetarian_itemframe);
+		
+		ShapedRecipe vegetarian_cake = new ShapedRecipe(new ItemStack(Material.CAKE, 1)).shape(new String[] { "AAA", "BCB", "DDD" }).setIngredient('A', Material.MILK_BUCKET).setIngredient('B', Material.SUGAR).setIngredient('C', Material.APPLE).setIngredient('D', Material.WHEAT);
+		getServer().addRecipe(vegetarian_cake);
 	}
 
   public void onDisable()
   {
-		getLogger().info("[Vegetarians Are Crazy][v0.1a] Plugin is disabled.");
+		getLogger().info("[v0.1.2] Plugin is disabled.");
   }
 }
